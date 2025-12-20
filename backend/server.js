2a8973556d01e30d651463db1953d220b8018c21
saminsiam4-coder <saminsiam@gmail.com>
2025-12-20 13:17:39 +0600

    server

--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const hostname = "127.0.0.1";
-
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 // Import routes
-const authRoutes = require('./routes/auth');
-const productRoutes = require('./routes/product');
-const cartRoutes = require('./routes/cart');
+const authRoutes = require('../backend/routes/auth');
+const productRoutes = require('../backend/routes/product');
+const cartRoutes = require('../backend/routes/cart');
 
 app.use('/api/auth', authRoutes);
-app.use('/api/product', productRoutes);
+app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
 
-const PORT = 5000;
-app.listen(PORT, hostname, () => {
-  console.log('Server running at http://${hostname}:${PORT}');
-});
+const PORT = 5500;
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
